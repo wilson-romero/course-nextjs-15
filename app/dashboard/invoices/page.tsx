0b35f16d890b09bb6a0ee1c62ec9a52adf347e7a
Wilson Romero <me@wilsonromero.com>
2025-03-09 10:33:03 -0500

    ✨: Implement pagination in invoices page; refactor search to reset page on new query

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,9 +2,10 @@ import Pagination from '@/app/ui/invoices/pagination'
 import Search from '@/app/ui/search'
 import Table from '@/app/ui/invoices/table'
 import { CreateInvoice } from '@/app/ui/invoices/buttons'
-import { ubuntu } from '@/app/ui/fonts'
+import { fetchInvoicesPages } from '@/app/lib/data'
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
 import { Suspense } from 'react'
+import { ubuntu } from '@/app/ui/fonts'
 
 export default async function Page(props: {
     searchParams?: Promise<{
@@ -15,6 +16,7 @@ export default async function Page(props: {
     const searchParams = await props.searchParams
     const query = searchParams?.query || ''
     const currentPage = Number(searchParams?.page) || 1
+    const totalPages = await fetchInvoicesPages(query)
 
     return (
         <div className="w-full">
@@ -29,7 +31,7 @@ export default async function Page(props: {
                 <Table query={query} currentPage={currentPage} />
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     )
