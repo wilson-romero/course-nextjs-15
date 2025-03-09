c8fb6aa3bc846adf3d08a5e487df6ce5ef14801a
Wilson Romero <me@wilsonromero.com>
2025-03-08 19:40:25 -0500

    ✨: Integrate Ubuntu font and update layout and logo components for improved styling

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Ubuntu } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const ubuntu = Ubuntu({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+})
