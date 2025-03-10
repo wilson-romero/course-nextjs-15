f0fec4a0f296e546dde5484d0abcfd09ede8adf8
Wilson Romero <me@wilsonromero.com>
2025-03-09 23:18:15 -0500

    ✨: Add metadata for Invoices page and update layout metadata for improved SEO

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import { fetchInvoicesPages } from '@/app/lib/data'
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
 import { Suspense } from 'react'
 import { ubuntu } from '@/app/ui/fonts'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Invoices',
+}
 
 export default async function Page(props: {
     searchParams?: Promise<{
